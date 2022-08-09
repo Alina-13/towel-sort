16cf2362d63e8d7ad625308411eb898ca3345220
Alina-13 <alina.za13@gmail.com>
2022-08-09 19:21:40 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  return matrix.reduce((accumulator, currentValue, index) => {
+    currentValue.sort((a, b) => !(index & 1) ? a - b : b - a).map(value => accumulator.push(value))
+    return accumulator
+  }, [])
 }
